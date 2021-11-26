6b333df9d2b403664445c0d0d4eaf95a422b4908
sahumo <7523460@gmail.com>
2021-11-26 19:53:28 +0200

    переменные

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 "use strict";
 
+let a = 5;
+b = a;
 
+b = b + 5;
+
+
+console.log(a);
+console.log(b);
+
+// Running] node "w:\КУРС ДЖАВА СКРИПТ\1\19 PEREDACHA_DANNIX_PO_SSILKE_ILI_ZNACHENIJU\tempCodeRunnerFile.js"
+// 5
+// 10

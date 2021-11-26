56bce1bc1ae809eb38a0a25708bc7fc53015ab53
sahumo <7523460@gmail.com>
2021-11-27 00:28:55 +0200

    в  const clone  лежит поверхностная копия  предыдущего  обьекта

--- script.js
+++ script.js
@@ -16,13 +16,16 @@ const add = {
     e : 20
 };
 
-console.log(Object.assign(numbers, add));
-
-///[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\19 PEREDACHA_DANNIX_PO_SSILKE_ILI_ZNACHENIJU\tempCodeRunnerFile.js"
-//{ a: 2, b: 5, c: { x: 7, y: 4 }, d: 17, e: 20 }
+const clone  = Object.assign({}, add);
 
 
+clone.d = 20;
 
+console.log(add);
+console.log(clone);
 
+//[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\19 PEREDACHA_DANNIX_PO_SSILKE_ILI_ZNACHENIJU\tempCodeRunnerFile.js"
+//{ d: 17, e: 20 }
+//{ d: 20, e: 20 }
 
 

7320d0a896e8929d303c77a4ddd780675d8f7969
sahumo <7523460@gmail.com>
2021-11-26 20:41:42 +0200

    создание  копии  обьекта  посредством  перебора  цикла

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
 "use strict";
 
-const obj = {
-a:5,
-b:1
+
+
+//эта  функция при помощи цикла создаст новый обьект перебирая старые свойства что уже были в обьекте
+function copy(mainObj) {  // аргумент  mainObj
+
+    let objCopy = {};
+
+    let key;// перебирающая конструкция  переменная  key  вне
+
+    for (key in mainObj) {// есть ключи внутри обьекта  mainObj 
+
+        objCopy[key] = mainObj[key];// делаем копию из нашего главного обьекта пройдемся по старому mainObj  скопируем  все  свойства  и  поместим  в  нашу  копию objCopy которая изначально была пустым обьектом
+
+    }
+
+
+    return objCopy;// возвращаем  нашу  копию  наружу  чтобы  как  нибудь  взаимодействовать
+}
+
+
+
+const numbers = {// это обьект
+
+    a: 2,
+    b: 5,
+    c: {// вложенный  обьект
+        x: 7,
+        y: 4
+
+
+    }
+
 };
 
-const copy = obj;
+const newNumbers = copy(numbers);// воспользуемся функцией copy она  примет  в  себя  обьект  numbers и вернет нам новый  обьект  который  уже  будет  копией
 
-copy.a = 10;
 
-console.log(copy);
-console.log(obj);
+newNumbers.a = 10;// обращаемся  к  свойству а
+
+console.log(newNumbers);
+console.log(numbers);
+
 
 // [Running] node "w:\КУРС ДЖАВА СКРИПТ\1\19 PEREDACHA_DANNIX_PO_SSILKE_ILI_ZNACHENIJU\tempCodeRunnerFile.js"
-// { a: 10, b: 1 }
-// { a: 10, b: 1 }
+
+// { a: 10, b: 5, c: { x: 7, y: 4 } } // скопированный   обьект  где  мы  поменяли  значение  а   на  10
+
+// { a: 2, b: 5, c: { x: 7, y: 4 } }//  базовый  обьект

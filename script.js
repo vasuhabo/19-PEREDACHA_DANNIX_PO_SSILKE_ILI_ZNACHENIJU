a73458115102e2741ff812fccd27085a0a1170bb
sahumo <7523460@gmail.com>
2021-11-27 00:39:27 +0200

    копирование массива

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
 "use strict";
 
-const numbers = {// это обьект
+const old_array = ['a','b','c'];
 
-    a: 2,
-    b: 5,
-    c: {// вложенный  обьект
-        x: 7,
-        y: 4
+const neo_array = old_array.slice();
 
-    }
-    };
+neo_array[1] = 'hfghgfkjkhfk';
 
-const add = {
-    d : 17,
-    e : 20
-};
+console.log (neo_array);
 
-const clone  = Object.assign({}, add);
+console.log(old_array);
 
 
-clone.d = 20;
-
-console.log(add);
-console.log(clone);
-
-//[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\19 PEREDACHA_DANNIX_PO_SSILKE_ILI_ZNACHENIJU\tempCodeRunnerFile.js"
-//{ d: 17, e: 20 }
-//{ d: 20, e: 20 }
-
-
+///[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\19 PEREDACHA_DANNIX_PO_SSILKE_ILI_ZNACHENIJU\tempCodeRunnerFile.js"
+///[ 'a', 'hfghgfkjkhfk', 'c' ]
+//[ 'a', 'b', 'c' ]
